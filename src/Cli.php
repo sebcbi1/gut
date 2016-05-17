276f058caed95b1abf9a79846abfc23b1328e846
sebcbi1 <sebcbi1@gmail.com>
2016-05-17 14:24:21 +0300

    wip

--- src/Cli.php
+++ src/Cli.php
@@ -51,20 +51,6 @@ class Cli
     public function parseCommandLineOptions()
     {
 
-        $this->term->arguments->add([
-            'location' => [
-                'prefix'       => 'l',
-                'longPrefix'   => 'location',
-                'description'  => 'Execute command only for the specified location'
-            ]
-        ]);
-
-        $this->term->arguments->parse();
-        $location = $this->term->arguments->get('location');
-        if (!empty($location)) {
-
-        }
-
         global $argv;
         $options = array_slice($argv, 1);
         $command = null;
